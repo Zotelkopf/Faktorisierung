3059a8b31cc5f94815db8d2344c461510f186240
Nicolas Flämig <117692237+Zotelkopf@users.noreply.github.com>
2022-11-08 15:04:40 +0100

    Update randomnum.py

--- randomnum.py
+++ randomnum.py
@@ -1,11 +1,22 @@
-from random import randint
+from random import randint, normalvariate
 import math
+from functions import next_prime
 
 
 # n Anzahl Faktoren, s1 min Stellenzahl, s2 max Stellenzahl
 def randomnum(n, s1, s2):
+  if n < s1:
+    return 0
   exp = []
-  exp.append(randint(s1, s2))
+  p = []
+  x = 1
+  s = randint(s1, s2)
   for i in range(n - 1):
     exp.append()
   
+
+  
+  for i in range(n):
+    p.append(normalvariate(10 ** exp[i]))
+    x *= p[i]
+  return x

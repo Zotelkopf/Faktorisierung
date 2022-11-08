a4e3f3587c41ef569dabe8bccc13a3c034c0041f
Nicolas Flämig <117692237+Zotelkopf@users.noreply.github.com>
2022-11-08 20:01:46 +0100

    Update randomnum.py

--- randomnum.py
+++ randomnum.py
@@ -5,22 +5,23 @@ from functions import nextprime_millerrabin
 
 # n Anzahl Faktoren, s1 min Stellenzahl, s2 max Stellenzahl
 def randomnum(n, s1, s2):
-  if n >= s1:
-    return 0
-  s = randint(s1, s2)
-  i1 = 1
-  i2 = 2
-  while i1 <= i2:
-    exp = []
-    p = []
-    x = 1
-    for i in range(n - 1):
-      exp.append(randint(1, (s - 1) // (n - i)))
-      s -= exp[i]  
-    exp.append(s - 1)
-    for i in range(n):
-      p.append(nextprime_millerrabin(int(10 ** exp[i] * lognormvariate(1, 1))))
-      x *= p[i]
-    if (len(str(x)) < s1 or len(str(x)) > s2) and i2 <= 5:
-      i2 += 1
+    if n >= s1:
+        return 0
+    i1 = 1
+    i2 = 1
+    while i1 <= i2:
+        i1 += 1
+        s = randint(s1, s2)
+        exp = []
+        p = []
+        x = 1
+        for i in range(n - 1):
+            exp.append(randint(1, (s - 1) // (n - i)))
+            s -= exp[i]  
+        exp.append(s - 1)
+        for i in range(n):
+            p.append(nextprime_millerrabin(int(10 ** exp[i] * lognormvariate(1, 1))))
+            x *= p[i]
+        if (len(str(x)) < s1 or len(str(x)) > s2) and i2 <= 10:
+            i2 += 1
   return x
